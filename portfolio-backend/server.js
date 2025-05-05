eeae0c1ae62b95c040e55dea69d8bdd6b948d5bc
Manmohan <mmaloo515@gmail.com>
2025-05-05 22:23:59 +0530

    Ready for first production

--- portfolio-backend/server.js
+++ portfolio-backend/server.js
@@ -83,6 +83,15 @@ app.post("/api/messages", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+const path = require("path");
+
+// Serve frontend from dist folder
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
