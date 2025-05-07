a1fde9bd311e09fc0620cef5e7f049e1a64424a7
Manmohan <mmaloo515@gmail.com>
2025-05-07 13:03:28 +0530

    Prepare for Render deployment

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -21,7 +21,7 @@ const Footer: React.FC = () => {
             LinkedIn
           </a>
           <a
-            href={`mailto:${import.meta.env.EMAIL_USER}`}
+            href={`mailto:${import.meta.env.VITE_EMAIL_USER}`}
             aria-label="Email"
             className="hover:text-blue-400"
           >
